5a74ca8abc6dad2282814b2380788d55f7094d9d
Kachornpat <67196986+Kachornpat@users.noreply.github.com>
2020-11-30 13:34:13 +0700

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -5,10 +5,13 @@ pygame.init()
 
 #set window
 screen = pygame.display.set_mode((800, 550))
+img = pygame.image.load("sudoku_img.png")
+pygame.display.set_icon(img)
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 gray = (200, 200, 200)
+dark_gray = (225, 225, 225)
 
 sudoku = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -39,6 +42,16 @@ def table(pos, dimension, color):
       for j in range(3):
          pygame.draw.rect(screen, color,
                           (pos[0] + dimension*i, pos[1] + dimension*j, dimension, dimension), 3)
+
+def update(value):
+   global number
+   pygame.draw.rect(screen, dark_gray, (enable_row*50 + 2, enable_column*50 + 2, 46, 46))
+   number = font.render(f"{value}", True, black)
+   screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
+   sudoku[enable_column - 1][enable_row - 1] = value        
+   pygame.display.update()
+   pygame.time.wait(100)
+
          
 # draw small 3x3 table 9 times
 for i in range(3):
@@ -64,103 +77,74 @@ while run:
          #enable box
          if pygame.mouse.get_pos()[0] > 50 and pygame.mouse.get_pos()[0] < 500 and pygame.mouse.get_pos()[1] > 50 and pygame.mouse.get_pos()[1] < 500:
             pygame.draw.rect(screen, white, (enable_row*50 + 2, enable_column*50 + 2, 46, 46))
-            pygame.draw.rect(screen,(225, 225, 225),
+            pygame.draw.rect(screen,dark_gray,
                              ((pygame.mouse.get_pos()[0]//50)*50 + 2,
                               (pygame.mouse.get_pos()[1]//50)*50 + 2, 46, 46))
-            enable_row = (pygame.mouse.get_pos()[0]//50)
-            enable_column = (pygame.mouse.get_pos()[1]//50)
-            enable = True
-      elif enable == True:
-         # unable box
-         if enable_row == pygame.mouse.get_pos()[0]//50 and enable_column == pygame.mouse.get_pos()[1]//50:
-            pygame.draw.rect(screen, white, (enable_row*50 + 2, enable_column*50 + 2, 46, 46))
-            enable = False
-         # enable another box
-         else:
-            pygame.draw.rect(screen, white, (enable_row*50 + 2, enable_column*50 + 2, 46, 46))
+
             if number != None and sudoku[enable_column-1][enable_row-1] != 0:
                number = font.render(f"{sudoku[enable_column-1][enable_row-1]}", True, black)
                screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-            pygame.draw.rect(screen,(225, 225, 225),
-                             ((pygame.mouse.get_pos()[0]//50)*50 + 2,
-                              (pygame.mouse.get_pos()[1]//50)*50 + 2, 46, 46))
+            
+            if number != None and sudoku[(pygame.mouse.get_pos()[1]//50)-1][(pygame.mouse.get_pos()[0]//50)-1] != 0:
+               number = font.render(f"{sudoku[(pygame.mouse.get_pos()[1]//50)-1][(pygame.mouse.get_pos()[0]//50)-1]}", True, black)
+               screen.blit(number, ((pygame.mouse.get_pos()[0]//50)*50 + 15, (pygame.mouse.get_pos()[1]//50)*50 + 10))
+            
             enable_row = (pygame.mouse.get_pos()[0]//50)
             enable_column = (pygame.mouse.get_pos()[1]//50)
+
+            enable = True
+
+      elif enable == True:
+         if pygame.mouse.get_pos()[0] > 50 and pygame.mouse.get_pos()[0] < 500 and pygame.mouse.get_pos()[1] > 50 and pygame.mouse.get_pos()[1] < 500:
+            # unable box
+            if enable_row == pygame.mouse.get_pos()[0]//50 and enable_column == pygame.mouse.get_pos()[1]//50:
+               pygame.draw.rect(screen, white, (enable_row*50 + 2, enable_column*50 + 2, 46, 46))
+               if number != None and sudoku[enable_column-1][enable_row-1] != 0:
+                  number = font.render(f"{sudoku[enable_column-1][enable_row-1]}", True, black)
+                  screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
+               enable = False
+               # print(number, sudoku[enable_column-1][enable_row-1])
+            # enable another box
+            else:
+               pygame.draw.rect(screen, white, (enable_row*50 + 2, enable_column*50 + 2, 46, 46))
+               pygame.draw.rect(screen,dark_gray,
+                              ((pygame.mouse.get_pos()[0]//50)*50 + 2,
+                                 (pygame.mouse.get_pos()[1]//50)*50 + 2, 46, 46))
+               if number != None and sudoku[enable_column-1][enable_row-1] != 0:
+                  number = font.render(f"{sudoku[enable_column-1][enable_row-1]}", True, black)
+                  screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
+
+               enable_row = (pygame.mouse.get_pos()[0]//50)
+               enable_column = (pygame.mouse.get_pos()[1]//50)
+               if number != None and sudoku[enable_column-1][enable_row-1] != 0:
+                  number = font.render(f"{sudoku[enable_column-1][enable_row-1]}", True, black)
+                  screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
          
       pygame.display.update()
       pygame.time.wait(300)
       
    if enable == True:
       if pygame.key.get_pressed()[pygame.K_KP1] or pygame.key.get_pressed()[pygame.K_1]:
-         number = font.render("1", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 1
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-             
+         update(1)  
       elif pygame.key.get_pressed()[pygame.K_KP2] or pygame.key.get_pressed()[pygame.K_2]:
-         number = font.render("2", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 2
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-         
+         update(2)
       elif pygame.key.get_pressed()[pygame.K_KP3] or pygame.key.get_pressed()[pygame.K_3]:
-         number = font.render("3", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 3
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-         
+         update(3)
       elif pygame.key.get_pressed()[pygame.K_KP4] or pygame.key.get_pressed()[pygame.K_4]:
-         number = font.render("4", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 4
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-         
+         update(4)
       elif pygame.key.get_pressed()[pygame.K_KP5] or pygame.key.get_pressed()[pygame.K_5]:
-         number = font.render("5", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 5
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-         
+         update(5)
       elif pygame.key.get_pressed()[pygame.K_KP6] or pygame.key.get_pressed()[pygame.K_6]:
-         number = font.render("6", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 6
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-         
+         update(6)
       elif pygame.key.get_pressed()[pygame.K_KP7] or pygame.key.get_pressed()[pygame.K_7]:
-         number = font.render("7", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 7
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-         
+         update(7)
       elif pygame.key.get_pressed()[pygame.K_KP8] or pygame.key.get_pressed()[pygame.K_8]:
-         number = font.render("8", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 8
-         print(sudoku)
-         pygame.display.update()
-         pygame.time.wait(300)
-         
+         update(8)
       elif pygame.key.get_pressed()[pygame.K_KP9] or pygame.key.get_pressed()[pygame.K_9]:
-         number = font.render("9", True, black)
-         screen.blit(number, (enable_row*50 + 15, enable_column*50 + 10))
-         sudoku[enable_column - 1][enable_row - 1] = 9
-         print(sudoku)
+         update(9)
+      elif pygame.key.get_pressed()[pygame.K_BACKSPACE] or pygame.key.get_pressed()[pygame.K_DELETE]:
+         pygame.draw.rect(screen, dark_gray, (enable_row*50 + 2, enable_column*50 + 2, 46, 46))
+         sudoku[enable_column - 1][enable_row - 1] = value        
          pygame.display.update()
-         pygame.time.wait(300)
-         
+         pygame.time.wait(100)
    
